f189000612031afcf7f9675fb402d238a147c92c
numax <numax@protonmail.com>
2017-11-07 23:31:49 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+
+//app.use is how you register middleware
+app.use(function(req, res, next) {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', function(err) {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+//app.use(function(req, res, next) {
+//    res.render('maintenance.hbs', {
+//        pageTitle: 'Site under construction'
+//        
+//    });
+//
+//});
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', function() {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', function(text) {
+    return text.toUpperCase();
+});
+//this is how routing is done in express
+app.get('/', function(req, res) {
+//    res.send("<h1>I'm working!</h1>");
+    
+    res.render('home.hbs', {
+        pageTitle: 'Velkommen',
+        welcomeMessage: 'Welcome to my Express Website!',
+        
+    });
+});
+
+app.get('/about', function(req, res) {
+    //res.render renders a view/template
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        
+    });
+});
+
+app.get('/bad', function(req, res) {
+    res.send({
+        errorMessage: "We weren't able to process your request"
+    })
+});
+
+app.listen(3000);
