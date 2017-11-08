0092eb1b407fe1197fa11bda9db31eb93aada323
numax <numax@protonmail.com>
2017-11-08 00:16:03 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
+
 
 var app = express();
 
@@ -65,4 +67,6 @@ app.get('/bad', function(req, res) {
     })
 });
 
-app.listen(3000);
+app.listen(port, function() {
+    console.log(`Server is running on port ${port}`);
+});
